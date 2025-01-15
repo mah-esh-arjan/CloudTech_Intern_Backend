ed891fa540dc634aa8c4afed30011e705bbc78e5
Mahesh maharjan <mah.dot98@gmail.com>
2025-01-15 09:30:36 +0545

    day2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,47 @@
 <?php
 
+use App\Http\Controllers\westController;
+use App\Http\Controllers\DatabaseController;
+use App\Http\Controllers\loginController;
+use App\Http\Middleware\RegisteredMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    
     return view('welcome');
 });
+
+Route::get('/hello',function() {
+    return 'hello world';
+});
+
+Route::get('/html',function () {
+    return '<h1> Hello from Html</h1>';
+});
+
+Route::get('/new',function () {
+
+    return view ('new');
+});
+
+// Route::get('/testnew', function(){
+
+//     return view ('testNew');
+    
+// });
+Route::get('/testnew', [westController::class, 'index']);
+Route::get('/error',function() {
+    return view ('error');
+});
+
+Route::get('/dashboard',function() {
+    return 'This is protected route';
+})->middleware('ProtectedMiddleware');
+
+Route::controller(loginController::class)->group(function(){
+    Route::get('/login','LoginView');
+    Route::post('/login','LoginClick');
+});
+
+
+Route::get('/testDatabase', [DatabaseController::class, 'index']);
