386f9d57a59457ddfafb4fc22e292fd20ad63e33
Mahesh maharjan <mah.dot98@gmail.com>
2025-01-21 14:37:57 +0545

    temporary testing

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -27,7 +27,7 @@ class StudentController extends Controller
 
         $path = $request->file('file')->store('public');
 
-        return $path;
+        // return $path;
 
         Student::create([
             'name' => $request->name,
