35a8817659f90680ca26f5e89b97d88906df84f1
Mahesh maharjan <mah.dot98@gmail.com>
2025-01-22 17:24:37 +0545

    day8

--- app/Models/movie.php
+++ app/Models/movie.php
@@ -7,6 +7,8 @@ use App\Models\Category;
 
 class Movie extends Model
 {
+    protected $fillable = ['name'];
+
     public function categories()
     {
         return $this->belongstoMany(Category::class, 'movie_category');
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,28 @@
 use App\Http\Controllers\DummyAPIController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get("data",[DummyAPIController::class,'getData']);
+Route::get("data", [DummyAPIController::class, 'getData']);
+
+Route::post("createMovie", [DummyAPIController::class, 'postData']);
+
+Route::get('/get-movie/{id}', [DummyAPIController::class, 'getMovie']);
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
+
+Route::post('/test-token/create', function () {
+    $user = User::find(1);
+    $token = $user->createToken('test-token')->plainTextToken;
+
+    return response()->json(['token' => $token]);
+});
